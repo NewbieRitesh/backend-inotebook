744f3115b71c02fc7b83e282f6cd21dea5c7498c
NewbieRitesh <newbieritesh@gmail.com>
2023-05-30 14:12:38 +0530

    updating to cloud database

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const mongoUri = "mongodb://127.0.0.1:27017/inotebook";
+const mongoUri = "mongodb+srv://nooblegendcoc:ritesh34567@ritesh-inotebook.fxr906h.mongodb.net/?retryWrites=true&w=majority";
 mongoose.set('strictQuery', true)
 
-const connectToMongo = () =>{
-    mongoose.connect(mongoUri, ()=>{
+const connectToMongo = () => {
+    mongoose.connect(mongoUri, () => {
         console.log("database is connected to our system");
     })
 }
